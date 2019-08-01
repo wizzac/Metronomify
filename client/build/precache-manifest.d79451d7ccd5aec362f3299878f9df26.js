43eea96a164afcc5bf172f1e1cf8d12a0a1c3daf
Wizzac <cuksebastian@gmail.com>
2019-07-31 21:09:43 -0300

    gregando build

---
+++ client/build/precache-manifest.d79451d7ccd5aec362f3299878f9df26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d80b6a063b47443f8766b6f7b8fa96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e9e49c949d7a90fc69",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4699aca44a3efb0b6326",
+    "url": "/static/css/main.f974b177.chunk.css"
+  },
+  {
+    "revision": "05e9e49c949d7a90fc69",
+    "url": "/static/js/2.36d24ccc.chunk.js"
+  },
+  {
+    "revision": "4699aca44a3efb0b6326",
+    "url": "/static/js/main.ec5aa416.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "248fa99a3a6aeffd2b6b1d639d960505",
+    "url": "/static/media/bg.248fa99a.png"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/static/media/click2.fe0ded65.wav"
+  }
+]);
